4173fa8a38ae2b16c2be5fe2e32aa7a3aaee03f5
MacTash <mactash076@gmail.com>
2025-11-17 14:02:11 +0530

    Update author information in __init__.py

--- tarot/__init__.py
+++ tarot/__init__.py
@@ -5,7 +5,7 @@ A mystical security tool that maps attack patterns to tarot archetypes.
 """
 
 __version__ = "0.1.0"
-__author__ = "Mayukh"
+__author__ = "Mayukh/MacTash"
 
 from .deck import TarotCard, MAJOR_ARCANA, MINOR_ARCANA
 from .engine import RedTeamTarot
